2ab09a510b6c97e1d76b25699726758dde8440a1
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-14 11:13:53 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Cooper Lawson on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
